ff2dfeb7388f641c2686c76dc06bbcfc6bf089b6
Saksham <gsaksham2000@gmail.com>
2022-08-15 23:36:44 +0530

    Added else to deal with app crashing due to wrong creds

--- routes/auth.js
+++ routes/auth.js
@@ -48,13 +48,15 @@ router.post("/login", async (req, res) => {
     }
     if(!user)
       res.status(400).json("Wrong credentials!");
-
-    const validated = await bcrypt.compare(req.body.password, user.password);
-    if(!validated)
-      res.status(400).json("Wrong credentials!");
-
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    else{
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      if(!validated)
+        res.status(400).json("Wrong credentials!");
+      else{
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+      }
+    }
   } 
   catch (err) {
     res.status(500).json(err);
